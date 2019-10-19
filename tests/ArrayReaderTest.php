6b8a17a1f2f8c69b5d375b428f31253914204ecb
odan <d.opitz@outlook.com>
2019-10-19 21:09:22 +0200

    Init

--- tests/ArrayReaderTest.php
+++ tests/ArrayReaderTest.php
@@ -2,6 +2,7 @@
 
 namespace Selective\Artifact\Test;
 
+use InvalidArgumentException;
 use PHPUnit\Framework\TestCase;
 use Selective\ArrayReader\ArrayReader;
 
@@ -25,6 +26,7 @@ class ArrayReaderTest extends TestCase
     public function testGetString($data, string $key, $default, $expected)
     {
         $reader = new ArrayReader($data);
+        static::assertSame($expected, $reader->findString($key, $default));
         static::assertSame($expected, $reader->getString($key, $default));
     }
 
@@ -37,6 +39,77 @@ class ArrayReaderTest extends TestCase
     {
         return [
             [['key' => 'value'], 'key', null, 'value'],
+            [['key' => null], 'key', 'value', 'value'],
+            [['key' => 'value'], 'nope', 'default', 'default'],
+            [['key' => ['key2' => 'value']], 'key.key2', null, 'value'],
+            [['key' => ['key2' => 'value']], 'key.nope', 'default', 'default'],
+        ];
+    }
+
+    /**
+     * Test.
+     *
+     * @dataProvider providerGetStringError
+     *
+     * @param mixed $data The data
+     * @param string $key The lookup key
+     *
+     * @return void
+     */
+    public function testGetStringError($data, string $key)
+    {
+        $this->expectException(InvalidArgumentException::class);
+
+        $reader = new ArrayReader($data);
+        $reader->getString($key);
+
+        static::assertTrue(true);
+    }
+
+    /**
+     * Provider.
+     *
+     * @return array[] The test data
+     */
+    public function providerGetStringError(): array
+    {
+        return [
+            [['key' => 'value'], 'nope'],
+            [['key' => null], 'nope'],
+            [['key' => ['key2' => 'value']], 'key.nope'],
+            [['key' => ['key2' => null]], 'key.key2'],
+        ];
+    }
+
+    /**
+     * Test.
+     *
+     * @dataProvider providerFindString
+     *
+     * @param mixed $data The data
+     * @param string $key The lookup key
+     * @param mixed $default The default value
+     * @param mixed $expected The expected value
+     *
+     * @return void
+     */
+    public function testFindString($data, string $key, $default, $expected)
+    {
+        $reader = new ArrayReader($data);
+        static::assertSame($expected, $reader->findString($key, $default));
+    }
+
+    /**
+     * Provider.
+     *
+     * @return array[] The test data
+     */
+    public function providerFindString(): array
+    {
+        return [
+            [['key' => 'value'], 'key', null, 'value'],
+            [['key' => null], 'key', 'value', 'value'],
+            [['key' => null], 'key', null, null],
             [['key' => 'value'], 'nope', 'default', 'default'],
             [['key' => ['key2' => 'value']], 'key.key2', null, 'value'],
             [['key' => ['key2' => 'value']], 'key.nope', 'default', 'default'],
